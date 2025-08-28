bf8437bd9091d25758ba3cc8e761889c43e505a4
logandapp <logandapp@gmail.com>
2025-08-28 19:58:45 -0400

    Implemented simple test maker.

--- lograder/tests/simple/test_maker.py
+++ lograder/tests/simple/test_maker.py
@@ -1,4 +1,7 @@
-from typing import Sequence, Optional
+from typing import Sequence, Optional, List
+
+from ..test import ComparisonTest
+from ..common.validation import validate_common_size
 
 def make_tests_from_files(
         *,  # kwargs-only; to avoid confusion with argument sequence.
@@ -6,6 +9,25 @@ def make_tests_from_files(
         inputs: Sequence[str],
         expected_outputs: Sequence[str],
         weights: Optional[Sequence[float]] = None # Defaults to equal-weight.
-):
-    # TODO: Write implementation of `make_tests_from_files` function.
-    ...
+) -> List[ComparisonTest]:
+
+    validate_common_size(
+        names = names,
+        inputs = inputs,
+        expected_outputs = expected_outputs,
+        weights = weights
+    )
+
+    if weights is None:
+        weights = [1.0 for _ in names]
+
+    generated_tests = []
+    for name, input_, expected_output, weight in zip(names, inputs, expected_outputs, weights, strict=True):
+        generated_tests.append(ComparisonTest(
+            name = name,
+            input = input_,
+            expected_output = expected_output,
+            weight = weight,
+        ))
+
+    return generated_tests
