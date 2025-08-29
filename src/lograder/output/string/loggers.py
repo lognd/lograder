58b66d6ecabfd254e00bcbd956312da8b192a019
logandapp <logandapp@gmail.com>
2025-08-29 00:26:58 -0400

    Switched to actual new, specific formatters.

--- src/lograder/output/string/loggers.py
+++ src/lograder/output/string/loggers.py
@@ -1,24 +1,33 @@
 from .formatters.assignment import (
-    AssignmentErrorFormatter,
-    AssignmentInfoFormatter,
     AssignmentStatsFormatter,
+    BuildErrorFormatter,
+    BuildInfoFormatter,
+    PreprocessorErrorFormatter,
+    PreprocessorInfoFormatter,
 )
-from .formatters.test_case import ReprFormatter, StatusFormatter, StreamFormatter
+from .formatters.test_case import (
+    ActualSTDOUTFormatter,
+    ExpectedSTDOUTFormatter,
+    ReprFormatter,
+    StatusFormatter,
+    STDERRFormatter,
+    STDINFormatter,
+)
 
 
 class AssignmentLogger:
-    _preprocessor_error_fmt = AssignmentErrorFormatter()
-    _preprocessor_info_fmt = AssignmentInfoFormatter()
-    _builder_error_fmt = AssignmentErrorFormatter()
-    _builder_info_fmt = AssignmentInfoFormatter()
+    _preprocessor_error_fmt = PreprocessorErrorFormatter()
+    _preprocessor_info_fmt = PreprocessorInfoFormatter()
+    _builder_error_fmt = BuildErrorFormatter()
+    _builder_info_fmt = BuildInfoFormatter()
     _runtime_summary_fmt = AssignmentStatsFormatter()
 
 
 class TestCaseLogger:
     _status_fmt = StatusFormatter()
-    _stdin_fmt = StreamFormatter()
-    _exp_stdout_fmt = StreamFormatter()
-    _act_stdout_fmt = StreamFormatter()
+    _stdin_fmt = STDINFormatter()
+    _exp_stdout_fmt = ExpectedSTDOUTFormatter()
+    _act_stdout_fmt = ActualSTDOUTFormatter()
     _exp_stdout_str_fmt = ReprFormatter()
     _act_stdout_str_fmt = ReprFormatter()
-    _stderr_fmt = StreamFormatter()
+    _stderr_fmt = STDERRFormatter()
