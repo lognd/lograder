eab05c07cb33097088f3d928cbd03f930ec737d1
logandapp <logandapp@gmail.com>
2025-08-28 23:28:54 -0400

    Added pydantic definitions for `AssignmentJSON`, `LeadreboardJSON`, `TestCaseJSON` according to the Gradescope Specifications.

---
+++ src/lograder/output/common/__init__.py
@@ -0,0 +1,7 @@
+from .types import Status, TextFormat, Visibility
+
+__all__ = [
+    "TextFormat",
+    "Visibility",
+    "Status",
+]
--- src/lograder/output/raw_json/test_case.py
+++ src/lograder/output/raw_json/test_case.py
@@ -1,5 +1,20 @@
-from pydantic import BaseModel
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
+from ..common import Status, TextFormat, Visibility
 
 
-class TestCase(BaseModel):
-    pass
+class TestCaseJSON(BaseModel):
+    score: Optional[float]
+    max_score: Optional[float]
+    execution_time: Optional[float] = Field(default=None, exclude=True)
+    status: Optional[Status] = None
+    name: Optional[str]
+    name_format: Optional[TextFormat] = "text"
+    number: Optional[str] = None
+    output: Optional[str]
+    output_format: Optional[TextFormat] = "ansi"
+    tags: List[str] = Field(default_factory=list)
+    visibility: Optional[Visibility] = "hidden"
+    extra_data: Optional[Dict[str, Any]] = Field(default_factory=dict)
