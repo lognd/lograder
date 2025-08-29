98cbcc94cef733c258a8688c46918a6f115c53d0
logandapp <logandapp@gmail.com>
2025-08-29 01:29:21 -0400

    Updated __init__ to contain more important submodule functions.

--- src/lograder/builder/common/__init__.py
+++ src/lograder/builder/common/__init__.py
@@ -1,8 +1,18 @@
+from .file_operations import (
+    bfs_walk,
+    is_cxx_source_file,
+    is_default_target,
+    is_executable,
+)
 from .interface import BuilderInterface, ResultInterface
 from .validation import validate_paths
 
 __all__ = [
     "BuilderInterface",
     "ResultInterface",
-    "validate_paths"
+    "validate_paths",
+    "bfs_walk",
+    "is_executable",
+    "is_default_target",
+    "is_cxx_source_file",
 ]
