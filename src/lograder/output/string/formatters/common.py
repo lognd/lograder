0d25c000204777caae237e847e2c94d513f105fb
logandapp <logandapp@gmail.com>
2025-08-29 00:21:05 -0400

    Moved `StreamFormatter` to `common.py`

--- src/lograder/output/string/formatters/common.py
+++ src/lograder/output/string/formatters/common.py
@@ -63,3 +63,14 @@ class ContextFormatter(FormatterInterface):
 
     def __str__(self):
         return f"{self._prefix}{self._content if isinstance(self._content, str) else self._content.to_string()}{self._suffix}"
+
+class StreamFormatter(ContextFormatter):  # type: ignore[call-arg]
+    def __init_subclass__(  # I override the __init_subclass__, but mypy doesn't know that.
+        cls, *args, stream_name: str, stream_color: str = Fore.CYAN, **kwargs
+    ):
+        super().__init_subclass__(
+            *args,
+            prefix=f"<{Style.BRIGHT}{stream_color}BEGIN {stream_name.upper()}{Fore.RESET}{Style.RESET_ALL}>\n",
+            suffix=f"\n<{Style.BRIGHT}{stream_color}END {stream_name.upper()}{Fore.RESET}{Style.RESET_ALL}>",
+            **kwargs,
+        )
--- src/lograder/output/string/formatters/test_case.py
+++ src/lograder/output/string/formatters/test_case.py
@@ -2,30 +2,18 @@ from typing import Union
 
 from colorama import Fore, Style
 
-from .common import ContextFormatter, FormatterInterface, RedText
+from .common import StreamFormatter, FormatterInterface
 
 
 class StatusFormatter:
     pass
 
 
-class StreamFormatter(ContextFormatter):  # type: ignore[call-arg]
-    def __init_subclass__(  # I override the __init_subclass__, but mypy doesn't know that.
-        cls, *args, stream_name: str, stream_color: str = Fore.CYAN, **kwargs
-    ):
-        super().__init_subclass__(
-            *args,
-            prefix=f"<{Style.BRIGHT}{stream_color}BEGIN {stream_name.upper()}{Fore.RESET}{Style.RESET_ALL}>\n",
-            suffix=f"\n<{Style.BRIGHT}{stream_color}END {stream_name.upper()}{Fore.RESET}{Style.RESET_ALL}>",
-            **kwargs,
-        )
-
-
-class ActualSTDOUTFormatter(StreamFormatter, stream_name="actual-stdout", stream_color=Fore.MAGENTA):
+class ActualSTDOUTFormatter(StreamFormatter, stream_name="actual stdout", stream_color=Fore.MAGENTA):
     pass
 
 
-class ExpectedSTDOUTFormatter(StreamFormatter, stream_name="expected-stdout", stream_color=Fore.GREEN):
+class ExpectedSTDOUTFormatter(StreamFormatter, stream_name="expected stdout", stream_color=Fore.GREEN):
     pass
 
 
