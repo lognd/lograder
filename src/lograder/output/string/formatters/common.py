f4b1087618baed2ee3993e0e6148cc54f99c87b1
logandapp <logandapp@gmail.com>
2025-08-28 23:59:54 -0400

    Began basic implementation of a couple Formatters.

--- src/lograder/output/string/formatters/common.py
+++ src/lograder/output/string/formatters/common.py
@@ -1,2 +1,35 @@
-class ContextFormatter:
-    pass
+from abc import ABC, abstractmethod
+from typing import Union
+
+
+class FormatterInterface(ABC):
+    @abstractmethod
+    def set(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    def to_string(self):
+        return self.__str__()
+
+
+class ContextFormatter(FormatterInterface):
+    _prefix: str = ""
+    _suffix: str = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._content: Union[FormatterInterface, str] = ""
+
+    def __init_subclass__(cls, *args, prefix: str, suffix: str, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        cls._prefix = prefix
+        cls._suffix = suffix
+
+    def set(self, *, content: Union[FormatterInterface, str] = "", **_):
+        self._content = content
+
+    def __str__(self):
+        return f"{self._prefix}{self._content if isinstance(self._content, str) else self._content.to_string()}{self._suffix}"
--- src/lograder/output/string/formatters/test_case.py
+++ src/lograder/output/string/formatters/test_case.py
@@ -1,13 +1,32 @@
-from .common import ContextFormatter
+from typing import Union
+from colorama import Fore, Style
+
+from .common import ContextFormatter, FormatterInterface
 
 
 class StatusFormatter:
     pass
 
 
-class StreamFormatter(ContextFormatter):
-    pass
+class StreamFormatter(ContextFormatter):  # type: ignore[call-arg]
+    def __init_subclass__(  # I override the __init_subclass__, but mypy doesn't know that.
+        cls, *args, stream_name: str, stream_color: Fore = Fore.CYAN, **kwargs
+    ):
+        super().__init_subclass__(
+            *args,
+            prefix=f"<{Style.BRIGHT}{stream_color}BEGIN {stream_name.upper()}{Fore.RESET}{Style.RESET_ALL}>\n",
+            suffix=f"\n<{Style.BRIGHT}{stream_color}END {stream_name.upper()}{Fore.RESET}{Style.RESET_ALL}>",
+            **kwargs,
+        )
 
 
-class ReprFormatter(ContextFormatter):
-    pass
+class ReprFormatter(FormatterInterface):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._content: Union[FormatterInterface, str] = ""
+
+    def set(self, *, content: Union[FormatterInterface, str] = "", **_):
+        self._content = content
+
+    def __str__(self):
+        return f"{repr(self._content if isinstance(self._content, str) else self._content.to_string())}"
