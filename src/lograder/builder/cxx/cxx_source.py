e4f2e4439a2f38ed80231a7037c15c4597311f01
logandapp <logandapp@gmail.com>
2025-08-29 01:24:55 -0400

    Renamed everything from `cpp` to `cxx`.

---
+++ src/lograder/builder/cxx/cxx_source.py
@@ -0,0 +1,48 @@
+import subprocess
+from pathlib import Path
+from typing import List, Union
+
+from ...common.types import FilePath
+from ...tests.test import ComparisonTest
+from ..common import BuilderInterface, bfs_walk, is_cxx_source_file
+from ..common.exceptions import GPPEntryPointNotFoundError
+from ..constants import (
+    DEFAULT_CXX_COMPILATION_FLAGS,
+    DEFAULT_CXX_STANDARD,
+    DEFAULT_EXECUTABLE_NAME,
+)
+
+
+class CxxSourceBuilder(BuilderInterface):
+    def __init__(self, project_root: FilePath):
+        self._sources: List[Path] = []
+        self._executable_path: Path = Path(project_root) / DEFAULT_EXECUTABLE_NAME
+        for file in bfs_walk(Path(project_root)):
+            if is_cxx_source_file(file):
+                self._sources.append(file)
+
+    def get_executable_path(self) -> Path:
+        return self._executable_path.resolve()
+
+    def preprocess(self):
+        pass
+
+    def build(self):
+        source_files: List[str] = [str(Path(path).resolve()) for path in self._sources]
+        cmd: List[Union[str, Path]] = [
+            "g++",
+            f"-std={DEFAULT_CXX_STANDARD}",
+            *DEFAULT_CXX_COMPILATION_FLAGS,
+            "-o",
+            self.get_executable_path(),
+            *source_files,
+        ]
+
+        proc = subprocess.run(cmd, capture_output=True, text=True)
+        if proc.returncode != 0:
+            raise GPPEntryPointNotFoundError(proc)
+
+        return Path(self.get_executable_path())
+
+    def run(self, test: ComparisonTest):
+        pass
