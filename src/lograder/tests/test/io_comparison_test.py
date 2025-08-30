a57f06a319f7a9064071f0ef90f887419c21fea3
logandapp <logandapp@gmail.com>
2025-08-29 21:52:18 -0400

    Added validation for test case.

--- src/lograder/tests/test/io_comparison_test.py
+++ src/lograder/tests/test/io_comparison_test.py
@@ -1,5 +1,7 @@
-from typing import Callable
+from time import perf_counter
+from typing import Callable, Optional
 
+from ..common.exceptions import TestNotRunError
 from .interface import TestInterface
 
 
@@ -10,22 +12,46 @@ class ComparisonTest(TestInterface):
         self._name: str = name
         self._input: str = input
         self._expected_output: str = expected_output
+        self._actual_output: Optional[str] = None
         self._weight: float = weight
 
+        self._start_time: Optional[float] = None
+        self._end_time: Optional[float] = None
+
+    def is_correct(self) -> bool:
+        return self.get_actual_output() == self._expected_output
+
     def is_correct_str(self, actual_output: str) -> bool:
-        return actual_output.strip() == self.get_expected_output().strip()
+        self._actual_output = actual_output
+        return self.is_correct()
 
     def is_correct_func(self, func: Callable[[str], str]):
-        return self.is_correct_str(func(self.get_input()))
+        self._start_time = perf_counter()
+        self._actual_output = func(self.get_input())
+        self._end_time = perf_counter()
+        return self.is_correct()
 
     def get_name(self) -> str:
         return self._name
+
+    def get_execution_time(self) -> Optional[float]:
+        if self._start_time is None or self._end_time is None:
+            return None
+        return self._end_time - self._start_time
+
+    def get_successful(self) -> bool:
+        return self.is_correct()
 
     def get_input(self) -> str:
         return self._input
 
     def get_expected_output(self) -> str:
         return self._expected_output
+
+    def get_actual_output(self) -> str:
+        if self._actual_output is None:
+            raise TestNotRunError(self.get_name())
+        return self._actual_output
 
     def get_weight(self) -> float:
         return self._weight
