8146e5ddbd3f6fad442695a81fe33755eba0e98d
logandapp <logandapp@gmail.com>
2025-08-30 16:22:08 -0400

    Added description to dummy build test.

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,5 +1,7 @@
+import pytest
+
 from lograder import hello_world
 
-
+@pytest.mark.description('Testing to see if package, `lograder`, built successfully.')
 def test_build():
     assert hello_world() == "Hello world! ~from `lograder`!"
