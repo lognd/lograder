97aaca909952c57af501b678f602248046e0a47c
logandapp <logandapp@gmail.com>
2025-08-29 21:52:40 -0400

    Implemented a fuller `TestInterface`.

--- src/lograder/tests/test/interface.py
+++ src/lograder/tests/test/interface.py
@@ -1,2 +1,31 @@
-class TestInterface:
-    pass
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class TestInterface(ABC):
+    @abstractmethod
+    def get_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_input(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_actual_output(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_successful(self) -> bool:
+        pass
+
+    @abstractmethod
+    def get_expected_output(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_weight(self) -> float:
+        pass
+
+    def get_execution_time(self) -> Optional[float]:
+        return None
