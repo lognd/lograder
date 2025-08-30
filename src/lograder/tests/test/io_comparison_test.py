2fd128883fcd9436392e464310d0696596be5426
logandapp <logandapp@gmail.com>
2025-08-30 01:03:49 -0400

    Started implementing analytics for io_comparison_test

--- src/lograder/tests/test/io_comparison_test.py
+++ src/lograder/tests/test/io_comparison_test.py
@@ -1,7 +1,8 @@
-from time import perf_counter
-from typing import Callable, Optional
+from typing import Callable, Optional, List
+import subprocess
 
 from ..common.exceptions import TestNotRunError
+from .analytics import MemoryLossSummary, WarningSummary, TimeSummary, CallSummary
 from .interface import TestInterface
 
 
@@ -10,34 +11,49 @@ class ComparisonTest(TestInterface):
         self, name: str, input: str, expected_output: str, weight: float = 1.0
     ):
         self._name: str = name
+        self._saved_cmd: Optional[List[str]] = None
         self._input: str = input
         self._expected_output: str = expected_output
         self._actual_output: Optional[str] = None
         self._weight: float = weight
 
-        self._start_time: Optional[float] = None
-        self._end_time: Optional[float] = None
+    def set_cmd(self, cmd: List[str]):
+        self._saved_cmd: List[str] = cmd
 
     def is_correct(self) -> bool:
         return self.get_actual_output() == self._expected_output
+
+    def is_correct_cmd(self, cmd: List[str]) -> bool:
+        self.set_cmd(cmd)
+        self._actual_output = subprocess.run(
+            cmd,
+            stdin=self.get_input(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
 
     def is_correct_str(self, actual_output: str) -> bool:
         self._actual_output = actual_output
         return self.is_correct()
 
     def is_correct_func(self, func: Callable[[str], str]):
-        self._start_time = perf_counter()
         self._actual_output = func(self.get_input())
-        self._end_time = perf_counter()
         return self.is_correct()
+
+    def get_warnings(self) -> Optional[WarningSummary]:
+        return None
+
+    def get_execution_time(self) -> Optional[TimeSummary]:
+        return None
+
+    def get_calls(self) -> Optional[List[CallSummary]]:
+        return None
+
+    def get_leaks(self) -> Optional[MemoryLossSummary]:
+        return None
 
     def get_name(self) -> str:
         return self._name
-
-    def get_execution_time(self) -> Optional[float]:
-        if self._start_time is None or self._end_time is None:
-            return None
-        return self._end_time - self._start_time
 
     def get_successful(self) -> bool:
         return self.is_correct()
