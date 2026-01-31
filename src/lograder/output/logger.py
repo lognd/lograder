83c1e3670d202039690c017a15d3205cfecb8049
logandapp <logandapp@gmail.com>
2026-01-31 14:49:58 -0500

    Implemented custom logger.

--- src/lograder/output/logger.py
+++ src/lograder/output/logger.py
@@ -5,12 +5,25 @@ import logging.config
 from pathlib import Path
 from typing import Optional
 
+from pydantic import BaseModel
+
+from .packets import wrap_packet
+
 try:
     import tomllib
 except ImportError:
     import tomli as tomllib  # type: ignore[no-redef]
 
 _PAST_SETUP: Optional[Path] = None
+
+
+class LograderLogger(logging.Logger):
+    PACKET_ATTR = "packet"
+
+    def packet(self, data: BaseModel, *, level: int = logging.INFO) -> None:
+        self.log(
+            level, data.__class__.__name__, extra={self.PACKET_ATTR: wrap_packet(data)}
+        )  # extra automatically unwraps dict into kwargs.
 
 
 def setup_logger(toml_file: Path = Path(__file__).parent / "config.toml") -> None:
