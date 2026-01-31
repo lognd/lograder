840e060da460a712460854587f4f2bb9ba2c2b57
logandapp <logandapp@gmail.com>
2026-01-31 18:33:28 -0500

    Implemented basic Rust `Result<T, E>` macro in Python.

---
+++ src/lograder/common/result.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from typing import Callable, Final, Generic, Optional, TypeVar, cast
+
+from ..exception import DeveloperException
+
+T = TypeVar("T")
+V = TypeVar("V")
+E = TypeVar("E")
+
+
+# noinspection PyPep8Naming
+class _EMPTY_OK:
+    __slots__ = ()
+
+
+# noinspection PyPep8Naming
+class _EMPTY_ERR:
+    __slots__ = ()
+
+
+# Stripped-down version of Rust's `Result<T, E>` macro; I figured this would be helpful.
+class Result(Generic[T, E]):
+    _OK_SINGLETON: Final[_EMPTY_OK] = _EMPTY_OK()
+    _ERR_SINGLETON: Final[_EMPTY_ERR] = _EMPTY_ERR()
+
+    def __init__(
+        self, *, ok: T | _EMPTY_OK = _OK_SINGLETON, err: E | _EMPTY_ERR = _ERR_SINGLETON
+    ) -> None:
+        self._ok: Final[T | _EMPTY_OK] = ok
+        self._err: Final[E | _EMPTY_ERR] = err
+        if self._ok is Result._OK_SINGLETON and self._err is Result._ERR_SINGLETON:
+            raise DeveloperException(
+                "There is a `Result` with neither an `ok` option specified or an `err` option specified."
+            )
+        elif (
+            self._ok is not Result._OK_SINGLETON
+            and self._err is not Result._ERR_SINGLETON
+        ):
+            raise DeveloperException(
+                f"There is a `Result` with both an `ok` option (type: `{self._ok.__class__.__name__}`, repr: `{self._ok}`) and an `err` option (type: `{self._err.__class__.__name__}`, repr: `{self._err}`) specified."
+            )
+
+    def map(self, func: Callable[[T], V]) -> Result[V, E]:
+        if self.is_err:
+            # cast is safe because invariant is only `err` or `ok` specified.
+            # if object `is_err`, then `ok` is empty anyway, and this is safe.
+            # noinspection PyUnnecessaryCast
+            return cast(Result[V, E], self)
+        # cast is safe because `is_err` being false guarantees that `_ok` is valid.
+        # noinspection PyUnnecessaryCast
+        return Result[V, E](ok=cast(T, func(self._ok)))
+
+    @property
+    def is_ok(self) -> bool:
+        return self._ok is not Result._OK_SINGLETON
+
+    @property
+    def ok(self) -> Optional[T]:
+        # cast is safe because `is_ok` guarantees that `_ok` is valid.
+        # noinspection PyUnnecessaryCast
+        return cast(T, self._ok) if self.is_ok else None
+
+    @property
+    def is_err(self) -> bool:
+        return self._err is not Result._ERR_SINGLETON
+
+    @property
+    def err(self) -> Optional[E]:
+        # cast is safe because `is_err` guarantees that `_err` is valid.
+        # noinspection PyUnnecessaryCast
+        return cast(E, self._err) if self.is_err else None
