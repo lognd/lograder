d54334e9bb6c412acb8a1d25935243aee555f0ad
logandapp <logandapp@gmail.com>
2026-02-01 00:42:21 -0500

    Added more functionality to `Result[T, E]`

--- src/lograder/common/result.py
+++ src/lograder/common/result.py
@@ -7,6 +7,7 @@ from ..exception import DeveloperException
 T = TypeVar("T")
 V = TypeVar("V")
 E = TypeVar("E")
+F = TypeVar("F")
 
 
 # noinspection PyPep8Naming
@@ -50,6 +51,49 @@ class Result(Generic[T, E]):
         # cast is safe because `is_err` being false guarantees that `_ok` is valid.
         # noinspection PyUnnecessaryCast
         return Result[V, E](ok=func(cast(T, self._ok)))
+
+    def map_err(self, func: Callable[[E], F]) -> Result[T, F]:
+        # casts are safe for the same reasons as above.
+        if self.is_ok:
+            # noinspection PyUnnecessaryCast
+            return cast(Result[T, F], self)
+        # noinspection PyUnnecessaryCast
+        return Result[T, F](err=func(cast(E, self._err)))
+
+    def and_then(self, func: Callable[[T], Result[V, F]]) -> Result[V, E | F]:
+        result = self.map(func)
+
+        # Lots of casting; couple are "unnecessary", but I do it just to appease
+        # any checker in the future that's *really* smart (i.e. no double singletons).
+        # Additionally, you can check manually but the invariants of `is_err` and
+        # `is_ok` ensures each of the following casts are safe.
+
+        if result.is_err:
+            # noinspection PyUnnecessaryCast
+            return Result[V, E | F](err=cast(E, result._err))
+        # noinspection PyUnnecessaryCast
+        ok = cast(Result[V, F], result._ok)
+        if ok.is_err:
+            # noinspection PyUnnecessaryCast
+            return Result[V, E | F](err=cast(F, ok._err))
+        # noinspection PyUnnecessaryCast
+        return Result[V, E | F](ok=cast(V, ok._ok))
+
+    def or_else(self, func: Callable[[E], Result[T, F]]) -> Result[T, F]:
+        if self.is_ok:
+            # noinspection PyUnnecessaryCast
+            return cast(Result[T, F], self)
+        # not `is_ok` invariant guarantees `err` cast validity
+        # noinspection PyUnnecessaryCast
+        return func(cast(E, self.err))
+
+    def inspect(self, func: Callable[[T], None]) -> Result[T, E]:
+        if not self.is_err:
+            # cast is safe because invariant is only `err` or `ok` specified.
+            # if object `is_err`, then `ok` is empty anyway, and this is safe.
+            # noinspection PyUnnecessaryCast
+            func(cast(T, self._ok))
+        return self
 
     @property
     def is_ok(self) -> bool:
