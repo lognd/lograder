635bae396eed5af7de2a4e304f91f9f810d8c656
logandapp <logandapp@gmail.com>
2026-02-07 15:32:46 -0500

    Edited I/O types; still trying to think through when an `Output` type would be useful.

--- src/lograder/pipeline/input.py
+++ src/lograder/pipeline/input.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import Any, Optional, Type
 
 from pydantic import BaseModel
 
 from ..common import Result
 from .package import Package
-from .step import Step
+from .step import _IS_ABSTRACT, Step
 
 
 class InputError(BaseModel):
@@ -12,5 +13,12 @@ class InputError(BaseModel):
 
 
 class Input(Step, ABC):
+    def __init_subclass__(
+        cls,
+        output_type: Optional[Type[Step]] | Type[_IS_ABSTRACT] = _IS_ABSTRACT,
+        **kwargs: Any,
+    ):
+        super().__init_subclass__([], output_type, **kwargs)
+
     @abstractmethod
     def __call__(self) -> Result[Package, InputError]: ...
