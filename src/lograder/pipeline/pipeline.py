1d2c309ff50f2a3e6b26fefbef9220f2108a7ab9
logandapp <logandapp@gmail.com>
2026-02-07 15:33:12 -0500

    Created basic pipeline logic.

--- src/lograder/pipeline/pipeline.py
+++ src/lograder/pipeline/pipeline.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
-from typing import Generic, Literal, NewType, cast
+from typing import Any, Generic
 
 from typing_extensions import TypeVar
 
-from ..common import Empty
+from ..common import Empty, Result
 from ..exception import StaffException
 from .block import Block
 from .build import Build
-from .check import Check
+from .check import Check, CheckData
+from .conversion import Conversion
 from .input import Input
+from .output import Output
 from .step import Step
-from .test import Test
+from .test import Test, TestData
 
 
 class PipelineState(Empty): ...
@@ -33,3 +35,48 @@ class Pipeline(Generic[T]):
         for step_prev, step_next in zip(steps[:-1], steps[1:]):
             step_next.assert_follow(step_prev.__class__)
         steps[-1].assert_end()
+        self._steps = steps
+
+    def __call__(self) -> None:
+        intermediate: Any = None
+        intermediate_result: Result[Any, Any]
+        check_data: list[CheckData] = []
+        test_data: list[TestData] = []
+        for step in self._steps:
+            # There seems to be a lot of "type-unsafety" in the following section,
+            # but remember that as long as every object actually follows its type
+            # contract, the beginning step should have alerted us to any mismatch,
+            # thus ignoring and `Any`-ing below should be safe-ish.
+            if isinstance(step, Input):
+                intermediate_result = step()
+                if intermediate_result.is_ok:
+                    intermediate = intermediate_result.danger_ok
+                else:
+                    # TODO: Fix handling!
+                    raise NotImplementedError
+            elif isinstance(step, Check):
+                check_result = step(intermediate)
+                check = check_result.ok
+                if check is not None:
+                    check_data.extend(check)
+            elif isinstance(step, Build):
+                intermediate_result = step(intermediate)
+                if intermediate_result.is_ok:
+                    intermediate = intermediate_result.danger_ok
+                else:
+                    # TODO: Fix handling!
+                    raise NotImplementedError
+            elif isinstance(step, Conversion):
+                intermediate_result = step(intermediate)
+                if intermediate_result.is_ok:
+                    intermediate = intermediate_result.danger_ok
+                else:
+                    # TODO: Fix handling!
+                    raise NotImplementedError
+            elif isinstance(step, Test):
+                test_result = step(intermediate)
+                test = test_result.ok
+                if test is not None:
+                    test_data.extend(test)
+            elif isinstance(step, (Block, Output)):
+                step()
