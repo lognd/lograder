f82e60878b7832b0df7724ea887e3806c4360921
logandapp <logandapp@gmail.com>
2026-02-09 11:46:06 -0500

    Created support for graceful exit to Pipeline with contexts.

---
+++ src/lograder/output/layout/control_layouts.py
@@ -0,0 +1,52 @@
+# noinspection PyPep8Naming
+# noinspection PyPep8Naming
+from colorama import Fore as F
+from colorama import Style as S
+
+from ...exception import DeveloperException, StaffException
+from ...pipeline.pipeline import PipelineError
+from ._base import Layout, register_layout
+
+
+@register_layout("pipeline-error")
+class PipelineErrorLayout(Layout[PipelineError]):
+    @classmethod
+    def to_ansi(cls, data: PipelineError) -> str:
+        output = []
+        if isinstance(data.error, StaffException):
+            output.append(
+                f"The autograder met an error that was likely caused by a {S.BRIGHT}misconfiguration by your course staff.{S.RESET_ALL}"
+            )
+        elif isinstance(data.error, DeveloperException):
+            output.append(
+                f"The autograder met an error that was most likely caused by a {S.BRIGHT}bug in the autograder source code.{S.RESET_ALL} Consult your course staff (so that they may coordinate with the developer). {S.BRIGHT}Please open an issue at `https://github.com/lognd/lograder/issues`{S.RESET_ALL} including this output, describing the bug and what lead up to it. Thanks!"
+            )
+        else:
+            output.append(
+                f"The autograder met a fatal error (of type `{data.error_type}`) that {S.BRIGHT}wasn't anticipated in any capacity by the autograder source code or the course staff.{S.RESET_ALL} Consult your course staff (so that they may coordinate with the developer). {S.BRIGHT}Please open an issue at `https://github.com/lognd/lograder/issues` as soon as possible{S.RESET_ALL} including this output, describing the bug and what lead up to it. Thanks!"
+            )
+        emsg_fmt = data.error_msg.replace("\n", "\n    ")
+        output.append(
+            f"The following error was reached:{F.RED}\n    {emsg_fmt}{F.RESET}"
+        )
+        output.append(
+            f"Here's the full traceback for the error:\n< {F.RED}BEGIN ERROR TRACEBACK{F.RESET} >{F.RED}"
+        )
+        output.append(data.error_traceback)
+        output.append(f"{F.RESET}\n< {F.RED}END ERROR TRACEBACK{F.RESET} >")
+        block_output = "\n".join(output)
+        return (
+            f"{S.BRIGHT}< {F.CYAN}ERROR WITH AUTOGRADER{F.RESET} >{S.RESET_ALL}\n"
+            f"{block_output}"
+        )
+
+    @classmethod
+    def to_simple(cls, data: PipelineError) -> str:
+        if isinstance(data.error, StaffException):
+            cause = "staff misconfiguration"
+        elif isinstance(data.error, DeveloperException):
+            cause = "autograder bug"
+        else:
+            cause = f"unanticipated fatal error ({data.error_type})"
+
+        return f"The pipeline met an error due to a {cause}; `{data.error_msg}`. Full traceback: {repr(data.error_traceback)}"
--- src/lograder/pipeline/pipeline.py
+++ src/lograder/pipeline/pipeline.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-from typing import Any
+import logging
+import traceback
+from contextlib import contextmanager
+from typing import Any, Iterable, Iterator
+
+from pydantic import BaseModel
 
 from ..common import Result
-from ..exception import StaffException
+from ..exception import DeveloperException, StaffException
+from ..output.logger import get_logger
 from .block import Block
 from .build import Build
 from .check import Check, CheckData
@@ -12,6 +18,33 @@ from .input import Input
 from .output import Output
 from .step import Step
 from .test import Test, TestData
+
+_LOGGER = get_logger("lograder.pipeline.pipeline")
+
+
+class PipelineError(BaseModel):
+    error: Exception
+    error_type: str
+    error_msg: str
+    error_traceback: str
+
+
+@contextmanager
+def graceful_pipeline_context() -> Iterator[None]:
+    try:
+        yield
+    except Exception as e:
+        _LOGGER.packet(
+            PipelineError(
+                error=e,
+                error_type=e.__class__.__name__,
+                error_msg=str(e),
+                error_traceback=traceback.format_exc(),
+            ),
+            level=logging.ERROR,
+        )
+    finally:
+        return
 
 
 class Pipeline:
@@ -27,45 +60,46 @@ class Pipeline:
         self._steps = steps
 
     def __call__(self) -> None:
-        intermediate: Any = None
-        intermediate_result: Result[Any, Any]
-        check_data: list[CheckData] = []
-        test_data: list[TestData] = []
-        for step in self._steps:
-            # There seems to be a lot of "type-unsafety" in the following section,
-            # but remember that as long as every object actually follows its type
-            # contract, the beginning step should have alerted us to any mismatch,
-            # thus ignoring and `Any`-ing below should be safe-ish.
-            if isinstance(step, Input):
-                intermediate_result = step()
-                if intermediate_result.is_ok:
-                    intermediate = intermediate_result.danger_ok
-                else:
-                    # TODO: Fix handling!
-                    raise NotImplementedError
-            elif isinstance(step, Check):
-                check_result = step(intermediate)
-                check = check_result.ok
-                if check is not None:
-                    check_data.extend(check)
-            elif isinstance(step, Build):
-                intermediate_result = step(intermediate)
-                if intermediate_result.is_ok:
-                    intermediate = intermediate_result.danger_ok
-                else:
-                    # TODO: Fix handling!
-                    raise NotImplementedError
-            elif isinstance(step, Conversion):
-                intermediate_result = step(intermediate)
-                if intermediate_result.is_ok:
-                    intermediate = intermediate_result.danger_ok
-                else:
-                    # TODO: Fix handling!
-                    raise NotImplementedError
-            elif isinstance(step, Test):
-                test_result = step(intermediate)
-                test = test_result.ok
-                if test is not None:
-                    test_data.extend(test)
-            elif isinstance(step, (Block, Output)):
-                step()
+        with graceful_pipeline_context():
+            intermediate: Any = None
+            intermediate_result: Result[Any, Any]
+            check_data: list[CheckData] = []
+            test_data: list[TestData] = []
+            for step in self._steps:
+                # There seems to be a lot of "type-unsafety" in the following section,
+                # but remember that as long as every object actually follows its type
+                # contract, the beginning step should have alerted us to any mismatch,
+                # thus ignoring and `Any`-ing below should be safe-ish.
+                if isinstance(step, Input):
+                    intermediate_result = step()
+                    if intermediate_result.is_ok:
+                        intermediate = intermediate_result.danger_ok
+                    else:
+                        # TODO: Fix handling!
+                        raise NotImplementedError
+                elif isinstance(step, Check):
+                    check_result = step(intermediate)
+                    check = check_result.ok
+                    if check is not None:
+                        check_data.extend(check)
+                elif isinstance(step, Build):
+                    intermediate_result = step(intermediate)
+                    if intermediate_result.is_ok:
+                        intermediate = intermediate_result.danger_ok
+                    else:
+                        # TODO: Fix handling!
+                        raise NotImplementedError
+                elif isinstance(step, Conversion):
+                    intermediate_result = step(intermediate)
+                    if intermediate_result.is_ok:
+                        intermediate = intermediate_result.danger_ok
+                    else:
+                        # TODO: Fix handling!
+                        raise NotImplementedError
+                elif isinstance(step, Test):
+                    test_result = step(intermediate)
+                    test = test_result.ok
+                    if test is not None:
+                        test_data.extend(test)
+                elif isinstance(step, (Block, Output)):
+                    step()
