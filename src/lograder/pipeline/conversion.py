fc13e3358aa959e47012441f793272618970ce02
logandapp <logandapp@gmail.com>
2026-02-27 12:37:15 -0500

    Strengthened the typing for `ConversionRegistry`.

--- src/lograder/pipeline/conversion.py
+++ src/lograder/pipeline/conversion.py
@@ -1,23 +1,22 @@
 from abc import ABC
 from typing import Any, TypeVar, Type
 
-from pydantic import BaseModel
-from typing import Callable
+from typing import Callable, cast
 
 from ..common import Result
 
 from ..exception import DeveloperException, StaffException
 
-T = TypeVar("T", bound=type)
-V = TypeVar("V", bound=type)
+A = TypeVar("A")
+B = TypeVar("B")
 
 class ConversionRegistry(ABC):
-    _registered_conversions: dict[tuple[type, type], Callable[[Any], Any]] = {}
+    _registered_conversions: dict[tuple[type, type], Callable[[Any], Result[Any, Any]]] = {}
     _f_map: dict[type, set[type]] = {}
     _r_map: dict[type, set[type]] = {}
 
     @classmethod
-    def register_conversion(cls, type_from: type, type_to: type, conversion: Callable[[Any], Any]):
+    def register_conversion(cls, type_from: Type[A], type_to: Type[B], conversion: Callable[[A], Result[B, Any]]) -> None:
         if (type_from, type_to) in cls._registered_conversions:
             raise DeveloperException(
                 f"Tried to register a second conversion (`{conversion.__qualname__}`) from `{type_from.__name__}` to `{type_to.__name__}`, but another conversion was already defined (`{cls._registered_conversions[(type_from, type_to)].__qualname__}`)."
@@ -27,7 +26,7 @@ class ConversionRegistry(ABC):
         cls._registered_conversions[(type_from, type_to)] = conversion
 
     @classmethod
-    def _error_helper(cls, type_from: type, type_to: type) -> str:
+    def _error_helper(cls, type_from: Type[A], type_to: Type[B]) -> str:
         msg = []
         if type_from in cls._f_map:
             msg.append(f"The only registered conversions from `{type_from.__name__}` are `{'`, `'.join(typ.__name__ for typ in cls.get_valid_targets(type_from))}`.")
@@ -40,13 +39,19 @@ class ConversionRegistry(ABC):
         return " ".join(msg)
 
     @classmethod
-    def get_conversion(cls, type_from: type, type_to: type) -> Callable[[Any], Any]:
+    def get_conversion(cls, type_from: Type[A], type_to: Type[B]) -> Callable[[A], Result[B, Any]]:
         if (type_from, type_to) not in cls._registered_conversions:
             raise StaffException(
                 f"Tried to get a conversion that doesn't exist (from `{type_from.__name__}` to `{type_to.__name__}`). "
                 + cls._error_helper(type_from, type_to)
             )
-        return cls._registered_conversions[(type_from, type_to)]
+        # This cast is necessary; I type-erased because class variables can't be generic.
+        # However, because of the invariance of this class (see the `register_conversion`
+        # method), we can safely say that the registered function has the correct type and
+        # can be narrow-casted.
+
+        # noinspection PyUnnecessaryCast
+        return cast(Callable[[A], Result[B, Any]], cls._registered_conversions[(type_from, type_to)])
 
     @classmethod
     def get_valid_targets(cls, type_from: type) -> frozenset[type]:
@@ -57,8 +62,9 @@ class ConversionRegistry(ABC):
         return frozenset(cls._r_map[type_to])
 
 
-A = TypeVar("A")
-B = TypeVar("B")
+def convert(type_from: Type[A], type_to: Type[B]) -> Callable[[A], Result[B, Any]]:
+    return ConversionRegistry.get_conversion(type_from, type_to)
+
 def register_conversion(type_from: Type[A], type_to: Type[B]) -> Callable[[Callable[[A], B]], Callable[[A], B]]:
     def wrapper(func: Callable[[A], Result[B, Any]]) -> Callable[[A], Result[B, Any]]:
         ConversionRegistry.register_conversion(type_from, type_to, func)
