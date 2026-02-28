c5a8f94ee7eac3a4d0c14aff0c07bc5019636528
logandapp <logandapp@gmail.com>
2026-02-27 22:56:51 -0500

    Added more utilities to the generic utilities.

--- src/lograder/common/__init__.py
+++ src/lograder/common/__init__.py
@@ -1,5 +1,5 @@
 from .empty import Empty
+from .generic_utils import get_first_bound_type, get_bound_types, unwrap_union_types
 from .result import Err, Ok, Result
-from .generic_utils import get_bound_type
 
-__all__ = ["Result", "Empty", "Ok", "Err", "get_bound_type"]
+__all__ = ["Result", "Empty", "Ok", "Err", "get_first_bound_type", "get_bound_types", "unwrap_union_types"]
--- src/lograder/common/generic_utils.py
+++ src/lograder/common/generic_utils.py
@@ -1,6 +1,22 @@
-from typing import Type, Any, get_args
+from typing import Any, Type, Union, Optional, get_args, get_origin
 
-def get_bound_type(cls: Type) -> Any:
-    for base in getattr(cls, "__orig_bases__", ()):
+
+def get_first_bound_type(typ: Type) -> Any:
+    for base in getattr(typ, "__orig_bases__", ()):
         return get_args(base)[0]
     return None
+
+def get_bound_types(cls: type, target_typ: Type) -> Optional[tuple[Any, ...]]:
+    for base in getattr(cls, "__orig_bases__", ()):
+        if get_origin(base) is not target_typ:
+            continue
+        args = get_args(base)
+        return tuple(args)
+    return None
+
+def unwrap_union_types(typ: Type) -> set[Type]:
+    origin = get_origin(typ)
+    if origin is Union:
+        return set(get_args(typ))
+    return {typ}
+
