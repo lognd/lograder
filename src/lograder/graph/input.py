2a07339bf4fd8ca716a096b5f8e67df09f7cff88
logandapp <logandapp@gmail.com>
2026-02-27 23:16:29 -0500

    Removed the unnecessary redefinitions of `__init_subclass__` and removed `Output` because in the architecture, it does not make sense.

--- src/lograder/graph/input.py
+++ src/lograder/graph/input.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 from ..common import Result
 from .package import Package
-from .step import _IS_ABSTRACT, Step
+from .step import Step
 
 
 class InputError(BaseModel):
@@ -13,12 +13,5 @@ class InputError(BaseModel):
 
 
 class Input(Step, ABC):
-    def __init_subclass__(
-        cls,
-        output_type: Optional[Type[Step]] | Type[_IS_ABSTRACT] = _IS_ABSTRACT,
-        **kwargs: Any,
-    ):
-        super().__init_subclass__([], output_type, **kwargs)
-
     @abstractmethod
     def __call__(self) -> Result[Package, InputError]: ...
