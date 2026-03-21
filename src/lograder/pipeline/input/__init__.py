770b73e8739a40c68bbd74c42e5e308d04337ad4
logandapp <logandapp@gmail.com>
2026-03-21 01:20:23 -0400

    Created a `LocalDirectory` input type.

---
+++ src/lograder/pipeline/input/__init__.py
@@ -0,0 +1,4 @@
+from .input import Input, InputData, InputError
+from .local_directory import LocalDirectory
+
+__all__ = ["Input", "InputData", "InputError", "LocalDirectory"]
---
+++ src/lograder/pipeline/input/local_directory.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import Generator
+
+from ...common import Err, Ok, Result, Unreachable
+from ...exception import UncaughtException
+from ..config import get_config
+from ..types.package import Manifest
+from ..types.pipeline_sentinel import PIPELINE_START
+from .input import Input
+
+
+class LocalDirectory(
+    Input[PIPELINE_START, Manifest, UncaughtException, Unreachable, Unreachable]
+):
+    def __init__(self, root: Path = get_config().root_directory) -> None:
+        self.root = root
+
+    def __call__(
+        self, _: PIPELINE_START
+    ) -> Generator[
+        Result[Unreachable, Unreachable],
+        None,
+        Result[Manifest, UncaughtException],
+    ]:
+        if False:
+            yield  # required for python to see this as a generator.
+        try:
+            manifest = Manifest.from_directory(self.root)
+            return Ok(manifest)
+        except Exception as e:
+            return Err(UncaughtException(error=e))
